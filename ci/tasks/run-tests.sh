9d35608a09c2bbf2a3f6be946fd114cc1fd26b38
Ava Silver <ava.silver@datadoghq.com>
2024-03-11 14:58:12 -0400

    use python image and run directly

--- ci/tasks/run-tests.sh
+++ ci/tasks/run-tests.sh
@@ -2,7 +2,13 @@
 
 set -euo pipefail
 
-image_tag=$(cat ci/image_tag.txt)
-readonly image_tag
+# image_tag=$(cat ci/image_tag.txt)
+# readonly image_tag
 
-time docker run $image_tag
+# time docker run $image_tag
+
+pip install -r build_requirements.txt
+
+cd diagnostic_settings_task
+pip install -r requirements.txt
+pytest tests
