19049e7d3a57b11950957aee8e25eabb6bb1f346
Ava Silver <ava.silver@datadoghq.com>
2024-03-18 15:16:30 -0400

    [AZINTS-2294] remove unnecessary newline at end

--- ci/tasks/coverage-comment.sh
+++ ci/tasks/coverage-comment.sh
@@ -7,7 +7,7 @@ curl -f -v 'https://pr-commenter.us1.ddbuild.io/internal/cit/pr-comment'\
     -X PATCH \
     -d '{
     "commit": "'"$CI_COMMIT_REF_NAME"'",
-    "message": "```\n'"$(awk -v ORS='\\n' '1' ci/coverage.txt)"'\n```",
+    "message": "```\n'"$(awk -v ORS='\\n' '1' ci/coverage.txt)"'```",
     "header": "Coverage Report",
     "org": "Datadog",
     "repo": "azure-log-forwarding-offering"
