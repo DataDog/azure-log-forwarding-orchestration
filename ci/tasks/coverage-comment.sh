8f11526839a9095a204a60ed733bf11ad3c9a008
Ava Silver <ava.silver@datadoghq.com>
2024-03-18 13:57:09 -0400

    [AZINTS-2294] see if we have coverage.txt in comment task

--- ci/tasks/coverage-comment.sh
+++ ci/tasks/coverage-comment.sh
@@ -2,5 +2,6 @@
 
 set -euxo pipefail
 
+cat ci/coverage.txt
 echo "[$CI_COMMIT_REF_NAME] - hello from pr-commenter" \
     | pr-commenter --header "Service pr-commenter test k8s" --for-pr $CI_COMMIT_REF_NAME
