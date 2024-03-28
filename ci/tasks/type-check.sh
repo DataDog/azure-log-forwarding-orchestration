a4981a2f42674150c41cfcf7d0a08b873400660f
Ava Silver <ava.silver@datadoghq.com>
2024-03-28 16:11:21 -0400

    [AZINTS-2206] simplify pip install for ci

--- ci/tasks/type-check.sh
+++ ci/tasks/type-check.sh
@@ -4,8 +4,7 @@ set -euxo pipefail
 
 # install mypy
 if [ "${CI:-}" == 'true' ]; then
-    pip install mypy==1.9.0
-    pip install -r diagnostic_settings_task/requirements.txt -r resources_task/requirements.txt
+    pip install mypy==1.9.0 -r diagnostic_settings_task/requirements.txt -r resources_task/requirements.txt
 fi
 
 python -m mypy diagnostic_settings_task resources_task
