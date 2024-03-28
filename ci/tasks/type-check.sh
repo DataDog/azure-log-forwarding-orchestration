ad285b861272123050b2b62cb5f2e85eabf73c1d
Ava Silver <ava.silver@datadoghq.com>
2024-03-28 16:16:06 -0400

    [AZINTS-2206] reset to main

--- ci/tasks/type-check.sh
+++ ci/tasks/type-check.sh
@@ -3,8 +3,7 @@
 set -euxo pipefail
 
 # install mypy
-if [ "${CI:-}" == 'true' ]; then
-    pip install mypy==1.9.0 -r diagnostic_settings_task/requirements.txt -r resources_task/requirements.txt
-fi
+pip install mypy==1.9.0
+pip install -r diagnostic_settings_task/requirements.txt -r resources_task/requirements.txt
 
 python -m mypy diagnostic_settings_task resources_task
