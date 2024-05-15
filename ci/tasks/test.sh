e6b5513b79fff04c87b7caf3a47e0b366a939fcb
Ava Silver <ava.silver@datadoghq.com>
2024-05-15 17:27:48 -0400

    [AZINTS-2451] clean up ci

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
-# run tests and coverage
+: run tests and coverage
 python -m coverage run -m pytest ./src
 
-# generate coverage report
+: generate coverage report
 python -m coverage report --skip-empty --show-missing > ci/coverage.txt
