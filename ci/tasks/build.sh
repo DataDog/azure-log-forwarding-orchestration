973125a918f7fad7ba6d7ba42e6cd935ee985d54
Ava Silver <ava.silver@datadoghq.com>
2024-05-15 11:09:43 -0400

    [AZINTS-2451] sudo

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 
 if [ "${CI:-}" == 'true' ]; then
-    apt-get install binutils
+    sudo apt-get install binutils
     pip install -U pyinstaller
     run-installer() {
         $HOME/.local/bin/pyinstaller $@
