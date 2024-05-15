e81178ae6f567a1605ebb6abfc0e8434d9b66c2c
Ava Silver <ava.silver@datadoghq.com>
2024-05-15 16:42:41 -0400

    [AZINTS-2451] use main image for everything

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -2,14 +2,8 @@
 
 set -euxo pipefail
 
-# install testing/coverage dependencies
-pip install pytest==8.0.2 coverage==7.4.4
-
-# install project dependencies
-find config/ -name 'requirements.txt' -exec pip install -r {} \;
-
 # run tests and coverage
-python -m coverage run -m pytest src
+python -m coverage run -m pytest ./src
 
 # generate coverage report
 python -m coverage report --skip-empty --show-missing > ci/coverage.txt
--- ci/tasks/type-check.sh
+++ ci/tasks/type-check.sh
@@ -2,12 +2,4 @@
 
 set -euxo pipefail
 
-# install mypy
-if [ "${CI:-}" == 'true' ]; then
-    pip install mypy==1.9.0 -r config/requirements.txt
-    for file in config/*/requirements.txt; do
-        pip install -r $file
-    done
-fi
-
-python -m mypy src
+python -m mypy ./src
