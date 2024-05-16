037eacfbd18eae504de35eb9929c6b999d7c47c9
Ava Silver <ava.silver@datadoghq.com>
2024-05-16 13:30:19 -0400

    [AZINTS-2451] update build.sh to have the correct contents

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -7,18 +7,27 @@ set -euxo pipefail
 
 for task in resources_task diagnostic_settings_task; do
     echo "Building $task"
+    mkdir -p ./dist/$task/$task
     pyinstaller \
         --onefile \
         --noconfirm \
         --nowindow \
         --clean \
-        --distpath ./dist/$task/ \
+        --distpath ./dist/$task/bin \
         --specpath ./specs \
         --log-level INFO \
         --paths ./src \
         ./src/tasks/$task.py
-    cp ./config/$task/* ./dist/$task/
-    cp ./config/host.json ./dist/$task/
+    cp ./dist/$task/bin/$task ./dist/$task/main
+    rm -rf ./dist/$task/bin
+    cp ./config/$task/function.json ./dist/$task/$task/function.json
+    cp ./config/host.json ./dist/$task/host.json
+    # if AzureWebJobsStorage is set, then update it in the local settings file
+    if [ -n "${AzureWebJobsStorage:-}" ]; then
+        jq ".Values.AzureWebJobsStorage = \"$AzureWebJobsStorage\"" ./config/local.settings.example.json > ./dist/$task/local.settings.json
+    else
+        cp ./config/local.settings.example.json ./dist/$task/local.settings.json
+    fi
     zip ./dist/$task.zip ./dist/$task/*
     echo "Built $task"
 done
