a6541507c35203d34275e2ff595325659f5b4c16
Ava Silver <ava.silver@datadoghq.com>
2024-06-28 11:19:34 -0400

    [AZINTS-2417] pull out diagnostic setting configurations

--- control_plane/cache/diagnostic_settings_cache.py
+++ control_plane/cache/diagnostic_settings_cache.py
@@ -30,6 +30,35 @@ DiagnosticSettingConfiguration: TypeAlias = (
 DiagnosticSettingsCache: TypeAlias = dict[str, dict[str, dict[str, DiagnosticSettingConfiguration]]]
 "Mapping of subscription_id to region to resource_id to DiagnosticSettingConfiguration"
 
+EVENT_HUB_DIAGNOSTIC_SETTING_CONFIGURATION: dict[str, Any] = {
+    "type": "object",
+    "properties": {
+        "id": {"type": "string"},
+        "type": {
+            "type": "string",
+            "const": EVENT_HUB_DIAGNOSTIC_SETTING_TYPE,
+        },
+        "event_hub_name": {"type": "string"},
+        "event_hub_namespace": {"type": "string"},
+    },
+    "required": ["id", "event_hub_name", "event_hub_namespace"],
+    "additionalProperties": False,
+}
+
+STORAGE_ACCOUNT_DIAGNOSTIC_SETTING_CONFIGURATION: dict[str, Any] = {
+    "type": "object",
+    "properties": {
+        "id": {"type": "string"},
+        "type": {
+            "type": "string",
+            "const": STORAGE_ACCOUNT_DIAGNOSTIC_SETTING_TYPE,
+        },
+        "storage_account_id": {"type": "string"},
+    },
+    "required": ["id", "storage_account_id"],
+    "additionalProperties": False,
+}
+
 DIAGNOSTIC_SETTINGS_CACHE_SCHEMA: dict[str, Any] = {
     "type": "object",
     "propertyNames": {"format": "uuid"},  # subscription_id
@@ -39,33 +68,8 @@ DIAGNOSTIC_SETTINGS_CACHE_SCHEMA: dict[str, Any] = {
             "type": "object",  # resource_id
             "additionalProperties": {
                 "oneOf": [
-                    {  # event hub diagnostic setting configuration
-                        "type": "object",
-                        "properties": {
-                            "id": {"type": "string"},
-                            "type": {
-                                "type": "string",
-                                "const": EVENT_HUB_DIAGNOSTIC_SETTING_TYPE,
-                            },
-                            "event_hub_name": {"type": "string"},
-                            "event_hub_namespace": {"type": "string"},
-                        },
-                        "required": ["id", "event_hub_name", "event_hub_namespace"],
-                        "additionalProperties": False,
-                    },
-                    {  # storage account diagnostic setting configuration
-                        "type": "object",
-                        "properties": {
-                            "id": {"type": "string"},
-                            "type": {
-                                "type": "string",
-                                "const": STORAGE_ACCOUNT_DIAGNOSTIC_SETTING_TYPE,
-                            },
-                            "storage_account_id": {"type": "string"},
-                        },
-                        "required": ["id", "storage_account_id"],
-                        "additionalProperties": False,
-                    },
+                    EVENT_HUB_DIAGNOSTIC_SETTING_CONFIGURATION,
+                    STORAGE_ACCOUNT_DIAGNOSTIC_SETTING_CONFIGURATION,
                 ],
             },
         },
