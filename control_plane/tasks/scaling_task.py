6dd5e2c9c64e276c1af50b66f1b825a807245d72
Ava Silver <ava.silver@datadoghq.com>
2024-07-05 18:34:42 -0400

    [AZINTS-2497] make log forwarder client

--- control_plane/tasks/scaling_task.py
+++ control_plane/tasks/scaling_task.py
@@ -3,15 +3,19 @@ from asyncio import gather, run
 from copy import deepcopy
 from json import dumps
 from logging import DEBUG, getLogger
-from typing import Any, Coroutine
+from typing import Any, Coroutine, AsyncContextManager, Self
+from uuid import UUID
 
 # 3p
-from azure.mgmt.web.aio import WebSiteManagementClient
-
+from azure.identity.aio import DefaultAzureCredential
+from azure.mgmt.web.v2023_12_01.aio import WebSiteManagementClient
+from azure.mgmt.web.v2023_12_01.models import AppServicePlan, SkuDescription
+from azure.mgmt.storage.v2023_05_01.aio import StorageManagementClient
+from azure.mgmt.storage.v2023_05_01.models import StorageAccountCreateParameters, Sku
 
 # project
 from cache.assignment_cache import ASSIGNMENT_CACHE_BLOB, deserialize_assignment_cache
-from cache.common import InvalidCacheError, read_cache, write_cache
+from cache.common import DiagnosticSettingConfiguration, InvalidCacheError, read_cache, write_cache
 from cache.resources_cache import RESOURCE_CACHE_BLOB, deserialize_resource_cache
 from tasks.task import Task, get_config_option, now
 
@@ -20,6 +24,52 @@ SCALING_TASK_NAME = "scaling_task"
 
 log = getLogger(SCALING_TASK_NAME)
 log.setLevel(DEBUG)
+
+
+class LogForwarderClient(AsyncContextManager):
+    def __init__(self, credential: DefaultAzureCredential, subscription_id: str, resource_group: str) -> None:
+        self.web_client = WebSiteManagementClient(credential, subscription_id)
+        self.storage_client = StorageManagementClient(credential, subscription_id)
+        self.subscription_id = subscription_id
+        self.resource_group = resource_group
+
+    async def __aenter__(self) -> Self:
+        await gather(self.web_client.__aenter__(), self.storage_client.__aenter__())
+        return self
+
+    async def __aexit__(self, *_) -> None:
+        await gather(self.web_client.__aexit__(), self.storage_client.__aexit__())
+        pass
+
+    async def create_log_forwarder(self, region: str) -> tuple[str, DiagnosticSettingConfiguration]:
+        log_forwarder_id = str(UUID())
+        # storage account
+        storage_account_name = f"log-forwarder-storage-{log_forwarder_id}"
+        storage_account_future = await self.storage_client.storage_accounts.begin_create(
+            resource_group_name=self.resource_group,
+            account_name=storage_account_name,
+            parameters=StorageAccountCreateParameters(sku=Sku(name="Standard_LRS"), kind="StorageV2", location=region),
+        )
+        # app service plan
+        app_service_plan_name = f"log-forwarder-plan-{log_forwarder_id}"
+        app_service_plan_future = await self.web_client.app_service_plans.begin_create_or_update(
+            self.resource_group,
+            app_service_plan_name,
+            AppServicePlan(
+                location=region, sku=SkuDescription(name="Y1", tier="Dynamic", size="Y1", family="Y", capacity=0)
+            ),
+        )
+        try:
+            await storage_account_future.result()
+            await app_service_plan_future.result()
+        except Exception:
+            log.exception("Failed to create log forwarder resources")
+            raise
+
+        # self.storage_client.blob_containers.create()
+        return log_forwarder_id, NotImplemented
+
+    async def delete_log_forwarder(self, region: str) -> str: ...
 
 
 class ScalingTask(Task):
@@ -51,21 +101,36 @@ class ScalingTask(Task):
         regions_to_add = current_regions - previous_region_assignments
         regions_to_remove = previous_region_assignments - current_regions
 
-        async with WebSiteManagementClient(self.credential, self.resource_group) as client:
+        async with LogForwarderClient(self.credential, subscription_id, self.resource_group) as client:
             tasks: list[Coroutine[Any, Any, None]] = []
             if regions_to_add:
-                tasks.extend(self.create_log_forwarder(client, subscription_id, region) for region in regions_to_add)
+                tasks.extend(self.create_log_forwarder(client, region) for region in regions_to_add)
             if regions_to_remove:
-                tasks.extend(self.delete_log_forwarder(client, subscription_id, region) for region in regions_to_remove)
+                tasks.extend(self.delete_log_forwarder(client, region) for region in regions_to_remove)
             await gather(*tasks)
 
         self.update_assignments(subscription_id)
 
-    async def create_log_forwarder(self, client: WebSiteManagementClient, subscription_id: str, region: str) -> None:
-        log.info("Creating log forwarder for subscription %s in region %s", subscription_id, region)
+    async def create_log_forwarder(
+        self,
+        client: LogForwarderClient,
+        region: str,
+    ) -> None:
+        log.info("Creating log forwarder for subscription %s in region %s", client.subscription_id, region)
+        config_id, configuration = await client.create_log_forwarder(region)
+        self.assignment_cache[client.subscription_id][region] = {
+            "configurations": {config_id: configuration},
+            "resources": {},
+        }
 
-    async def delete_log_forwarder(self, client: WebSiteManagementClient, subscription_id: str, region: str) -> None:
-        log.info("Deleting log forwarder for subscription %s in region %s", subscription_id, region)
+    async def delete_log_forwarder(
+        self,
+        client: LogForwarderClient,
+        region: str,
+    ) -> None:
+        log.info("Deleting log forwarder for subscription %s in region %s", client.subscription_id, region)
+        config_id = await client.delete_log_forwarder(region)
+        self.assignment_cache[client.subscription_id][region]["configurations"].pop(config_id, None)
 
     def update_assignments(self, sub_id: str) -> None:
         for region_config in self.assignment_cache[sub_id].values():
