781860aa88f68b17dcadfab97f3296bdf35002a3
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-09 10:49:39 -0400

    testing mocks

--- forwarder/internal/storage/client.go
+++ forwarder/internal/storage/client.go
@@ -1,12 +1,17 @@
 package storage
 
 import (
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"context"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 )
 
 // AzureBlobClient wraps around the azblob.Client struct, to allow for mocking.
 // these are the inherited and used methods.
 type AzureBlobClient interface {
-	NewListContainersPager(o *azblob.ListContainersOptions) *runtime.Pager[azblob.ListContainersResponse]
+	NewListContainersPager(o *azblob.ListContainersOptions) AzurePager[azblob.ListContainersResponse]
+}
+
+type AzurePager[T any] interface {
+	NextPage(ctx context.Context) (T, error)
+	More() bool
 }
