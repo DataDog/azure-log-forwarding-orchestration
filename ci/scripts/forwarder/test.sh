490b2ab9d0ef0c17af9f7f64fe56069555e4decd
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-10 14:37:31 -0400

    use installed cobertura

--- ci/scripts/forwarder/test.sh
+++ ci/scripts/forwarder/test.sh
@@ -5,6 +5,5 @@ cd forwarder
 go test -coverprofile=forwarder_coverage.txt -race -json -v ./...
 go get -v -u github.com/boumenot/gocover-cobertura
 go install github.com/boumenot/gocover-cobertura
-find / -name gocover-cobertura
-gocover-cobertura < forwarder_coverage.txt > forwarder_coverage.xml
+/root/go/bin/gocover-cobertura < forwarder_coverage.txt > forwarder_coverage.xml
 mv forwarder_coverage.xml ../ci/
