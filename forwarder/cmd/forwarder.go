2090f5f053eb21cd704c5a61d0f1bae00c1a7601
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-10 18:40:49 -0400

    add integration test

--- forwarder/cmd/forwarder.go
+++ forwarder/cmd/forwarder.go
@@ -6,12 +6,13 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	"github.com/DataDog/azure-log-forwarding-orchestration/forwarder/internal/storage"
 	"golang.org/x/sync/errgroup"
+	"io"
 	"log"
 	"os"
 	"time"
 )
 
-func RunWithClient(client *storage.Client) {
+func Run(client *storage.Client, output io.Writer) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	_, ctx = errgroup.WithContext(ctx)
@@ -19,29 +20,25 @@ func RunWithClient(client *storage.Client) {
 	// Get containers with logs from storage account
 	containers, err := client.GetContainersMatchingPrefix(ctx, storage.LogContainerPrefix)
 	if err != nil {
-		log.Println(fmt.Errorf("error getting contains with prefix %s: %v", storage.LogContainerPrefix, err))
+		output.Write([]byte(fmt.Sprintf("error getting contains with prefix %s: %v", storage.LogContainerPrefix, err)))
 		return
 	}
 	for _, container := range containers {
-		log.Println(fmt.Sprintf("Container: %s", *container))
+		output.Write([]byte(fmt.Sprintf("Container: %s", *container)))
 	}
-}
-
-func Run(storageAccountConnectionString string) {
-	client, err := storage.NewClient(storageAccountConnectionString, &azblob.ClientOptions{})
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	RunWithClient(client)
 }
 
 func main() {
 	start := time.Now()
 	log.Println(fmt.Sprintf("Start time: %v", start.String()))
 	storageAccountConnectionString := os.Getenv("AzureWebJobsStorage")
-	Run(storageAccountConnectionString)
+	client, err := storage.NewClient(storageAccountConnectionString, &azblob.ClientOptions{})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	Run(client, log.Writer())
 	log.Println(fmt.Sprintf("Run time: %v", time.Since(start).String()))
 	log.Println(fmt.Sprintf("Final time: %v", (time.Now()).String()))
 }
---
+++ forwarder/cmd/forwarder_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/DataDog/azure-log-forwarding-orchestration/forwarder/internal/storage"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/dnaeon/go-vcr.v3/cassette"
+	"gopkg.in/dnaeon/go-vcr.v3/recorder"
+	"os"
+	"path"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	// Integration test for the storage forwarder
+	// GIVEN
+	rec, err := recorder.New(path.Join("fixtures", "run"))
+	assert.NoErrorf(t, err, "failed creating recorder ")
+	defer rec.Stop()
+
+	hook := func(i *cassette.Interaction) error {
+		delete(i.Request.Headers, "Authorization")
+		return nil
+	}
+	rec.AddHook(hook, recorder.AfterCaptureHook)
+
+	storageAccountConnectionString := os.Getenv("AzureWebJobsStorage")
+
+	clientOptions := &azblob.ClientOptions{}
+	clientOptions.Transport = rec.GetDefaultClient()
+	client, err := storage.NewClient(storageAccountConnectionString, clientOptions)
+	assert.NoError(t, err)
+
+	var output []byte
+	buffer := bytes.NewBuffer(output)
+
+	// WHEN
+	Run(client, buffer)
+
+	// THEN
+	got := string(buffer.Bytes())
+	assert.Contains(t, got, "insights-logs-functionapplogs")
+}
