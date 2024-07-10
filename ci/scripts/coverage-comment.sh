81f8a14a260b041a85f7b9651d8c9776a44f39ba
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-10 18:15:43 -0400

    go back to one comment job

--- ci/scripts/coverage-comment.sh
+++ ci/scripts/coverage-comment.sh
@@ -7,7 +7,7 @@ curl -f -v 'https://pr-commenter.us1.ddbuild.io/internal/cit/pr-comment'\
     -X PATCH \
     -d '{
     "commit": "'"$CI_COMMIT_REF_NAME"'",
-    "message": "'"$1"' Coverage:\n'"$(awk -v ORS='\\n' '1' "$2")"'",
+    "message": "Control Plane Coverage:\n'"$(awk -v ORS='\\n' '1' "ci/control_plane_coverage.md")"'\nForwarder Coverage:'"$(awk -v ORS='\\n' '1' "ci/forwarder_coverage.md")"'",
     "header": "Coverage Report",
     "org": "Datadog",
     "repo": "azure-log-forwarding-offering"
