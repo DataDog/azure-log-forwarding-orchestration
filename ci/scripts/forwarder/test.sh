501d8ba05177ac748f44f6f505a2e500bff3b8e6
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-10 14:30:28 -0400

    add verbose get

--- ci/scripts/forwarder/test.sh
+++ ci/scripts/forwarder/test.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 cd forwarder
 go test -coverprofile=forwarder_coverage.txt -race -json -v ./...
-go get github.com/boumenot/gocover-cobertura
-/root/go/pkg/mod/cache/download/github.com/boumenot/gocover-cobertura < forwarder_coverage.txt > forwarder_coverage.xml
+go get -v -u github.com/boumenot/gocover-cobertura
+gocover-cobertura < forwarder_coverage.txt > forwarder_coverage.xml
 mv forwarder_coverage.xml ../ci/
