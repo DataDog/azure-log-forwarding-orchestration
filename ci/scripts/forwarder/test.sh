ddf0eb55856713428dd2244fecbc1ee4bf55c858
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-10 14:32:12 -0400

    more trying to find it

--- ci/scripts/forwarder/test.sh
+++ ci/scripts/forwarder/test.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 cd forwarder
 go test -coverprofile=forwarder_coverage.txt -race -json -v ./...
 go get -v -u github.com/boumenot/gocover-cobertura
+go install github.com/boumenot/gocover-cobertura
+find / -name gocover-cobertura
 gocover-cobertura < forwarder_coverage.txt > forwarder_coverage.xml
 mv forwarder_coverage.xml ../ci/
