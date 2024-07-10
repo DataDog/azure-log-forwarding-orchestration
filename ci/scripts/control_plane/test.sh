b4b8474f2acdca4636dfadf4eeb6033772330016
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-10 17:26:07 -0400

    return installs to testing scripts

--- ci/scripts/control_plane/test.sh
+++ ci/scripts/control_plane/test.sh
@@ -10,4 +10,6 @@ python -m coverage run -m pytest ./control_plane
 
 : generate coverage report
 python -m coverage xml --skip-empty -o ci/control_plane_coverage.xml
+
+pip install pycobertura
 pycobertura show --format markdown ci/control_plane_coverage.xml > ci/control_plane_coverage.md
--- ci/scripts/forwarder/test.sh
+++ ci/scripts/forwarder/test.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 cd forwarder
 go test -coverprofile=forwarder_coverage.txt -race -json -v ./...
+go get -v -u github.com/boumenot/gocover-cobertura
+go install github.com/boumenot/gocover-cobertura
 /root/go/bin/gocover-cobertura < forwarder_coverage.txt > forwarder_coverage.xml
 
 source /venv/bin/activate
