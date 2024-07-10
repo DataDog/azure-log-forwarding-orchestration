69bcc4cc7e06fd5cbc4bd12c467f650344cff56a
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-10 14:41:10 -0400

    fix dependencies

--- ci/scripts/control_plane/test.sh
+++ ci/scripts/control_plane/test.sh
@@ -9,4 +9,4 @@ set -euxo pipefail
 python -m coverage run -m pytest ./control_plane
 
 : generate coverage report
-python -m coverage xml --skip-empty --show-missing > ci/control_plane_coverage.xml
+python -m coverage xml --skip-empty > ci/control_plane_coverage.xml
