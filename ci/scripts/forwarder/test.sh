c57cdf7f0c181e1a6f1508ab45e60165e6481f3f
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-10 14:18:09 -0400

    adding a find

--- ci/scripts/forwarder/test.sh
+++ ci/scripts/forwarder/test.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 cd forwarder
 go test -coverprofile=forwarder_coverage.txt -race -json -v ./...
 go get github.com/boumenot/gocover-cobertura
+find / -name gocover-cobertura
 gocover-cobertura < forwarder_coverage.txt > forwarder_coverage.xml
 mv forwarder_coverage.xml ../ci/
