57624655202ce5ae46f82014bd11664b612c8a6b
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-10 16:42:19 -0400

    needs path to cobertura

--- ci/scripts/forwarder/test.sh
+++ ci/scripts/forwarder/test.sh
@@ -5,8 +5,9 @@ cd forwarder
 go test -coverprofile=forwarder_coverage.txt -race -json -v ./...
 go get -v -u github.com/boumenot/gocover-cobertura
 go install github.com/boumenot/gocover-cobertura
-gocover-cobertura < forwarder_coverage.txt > forwarder_coverage.xml
+/root/go/bin/gocover-cobertura < forwarder_coverage.txt > forwarder_coverage.xml
 
+source /venv/bin/activate
 pip install pycobertura
 pycobertura show --format markdown forwarder_coverage.xml > forwarder_coverage.md
 mv forwarder_coverage.md ../ci/
