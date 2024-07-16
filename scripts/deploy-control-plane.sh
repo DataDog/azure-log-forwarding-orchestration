d8870990111cad6f2d1ed5f06389369b66539507
Ava Silver <ava.silver@datadoghq.com>
2024-07-16 18:38:18 -0400

    update logging

--- scripts/deploy-control-plane.sh
+++ scripts/deploy-control-plane.sh
@@ -4,30 +4,38 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <resource_group>"
     exit 1
 fi
-set -euxo pipefail
+set -euo pipefail
 resource_group=$1
 
 cd ./control_plane
 tasks="$(python -m tasks)"
-cd -
+cd ..
 
+echo -n "Checking for current function apps..."
 existing_functions="$(az functionapp list -g $resource_group | jq -r '.[].name')"
-storage_account="$(az storage account list -g $resource_group | jq -r '.[].name' | cut -d$'\n' -f1)"
+echo Done.
 
+echo -n "Checking for a storage account..."
+storage_account="$(az storage account list -g $resource_group | jq -r '.[].name' | cut -d$'\n' -f1)"
 if [[ -z "$storage_account" ]]; then
-    echo "Storage account does not exist, creating one"
+    echo -n "Storage account does not exist, creating one..."
     az storage account create --name lfo1234 --resource-group $resource_group --location eastus --sku Standard_LRS
     storage_account="lfo1234"
 fi
+echo Done.
 
 for task in $tasks; do
     function_app_name="${task//_/-}"
     if [[ "$existing_functions" != *"$function_app_name"* ]]; then
-        echo "Function app $function_app_name does not exist"
+        echo -n "Function app $function_app_name does not exist, creating one..."
         az functionapp create --resource-group $resource_group --consumption-plan-location eastus --name $function_app_name --storage-account $storage_account --runtime custom --functions-version 4 --os-type Linux
-        echo "Function app $function_app_name created"
+        echo Done.
+    fi
+    if [[ ! -d "./dist/$task" ]]; then
+        echo "Task $task has not been built, skipping."
+        continue
     fi
     cd ./dist/$task
     func azure functionapp publish $function_app_name --custom
-    cd -
+    cd ../..
 done
