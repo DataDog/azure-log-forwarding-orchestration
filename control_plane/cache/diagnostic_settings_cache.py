87b9d97b30c1131a98b59a5c8bf0be866d4c2a33
Ava Silver <ava.silver@datadoghq.com>
2024-07-17 10:59:36 -0400

    consolidate common cache functions and definitions (#27)
    * diagnostic settings cache
    
    * resource cache
    
    * consolidate common cache functions and definitions
    
    * imports
    
    * scaling task doesnt exist yet
    
    * remove unecesary type check
    
    * dont add none return type

--- control_plane/cache/diagnostic_settings_cache.py
+++ control_plane/cache/diagnostic_settings_cache.py
@@ -1,61 +1,13 @@
 from json import JSONDecodeError, loads
-from typing import Any, Literal, TypeAlias, TypedDict
+from typing import Any, TypeAlias
 
 from jsonschema import ValidationError, validate
+
+from cache.common import DIAGNOSTIC_SETTING_CONFIGURATION_SCHEMA, DiagnosticSettingConfiguration
 
 
 DIAGNOSTIC_SETTINGS_CACHE_BLOB = "settings.json"
 
-EVENT_HUB_DIAGNOSTIC_SETTING_TYPE = "eventhub"
-STORAGE_ACCOUNT_DIAGNOSTIC_SETTING_TYPE = "storageaccount"
-
-
-class EventHubDiagnosticSettingConfiguration(TypedDict, total=True):
-    id: str
-    type: Literal["eventhub"]
-    event_hub_name: str
-    event_hub_namespace: str
-
-
-EVENT_HUB_DIAGNOSTIC_SETTING_CONFIGURATION_SCHEMA: dict[str, Any] = {
-    "type": "object",
-    "properties": {
-        "id": {"type": "string"},
-        "type": {
-            "type": "string",
-            "const": EVENT_HUB_DIAGNOSTIC_SETTING_TYPE,
-        },
-        "event_hub_name": {"type": "string"},
-        "event_hub_namespace": {"type": "string"},
-    },
-    "required": ["id", "event_hub_name", "event_hub_namespace"],
-    "additionalProperties": False,
-}
-
-
-class StorageAccountDiagnosticSettingConfiguration(TypedDict, total=True):
-    id: str
-    type: Literal["storageaccount"]
-    storage_account_id: str
-
-
-STORAGE_ACCOUNT_DIAGNOSTIC_SETTING_CONFIGURATION_SCHEMA: dict[str, Any] = {
-    "type": "object",
-    "properties": {
-        "id": {"type": "string"},
-        "type": {
-            "type": "string",
-            "const": STORAGE_ACCOUNT_DIAGNOSTIC_SETTING_TYPE,
-        },
-        "storage_account_id": {"type": "string"},
-    },
-    "required": ["id", "storage_account_id"],
-    "additionalProperties": False,
-}
-
-DiagnosticSettingConfiguration: TypeAlias = (
-    EventHubDiagnosticSettingConfiguration | StorageAccountDiagnosticSettingConfiguration
-)
 
 DiagnosticSettingsCache: TypeAlias = dict[str, dict[str, DiagnosticSettingConfiguration]]
 "Mapping of subscription_id to resource_id to DiagnosticSettingConfiguration"
@@ -66,12 +18,7 @@ DIAGNOSTIC_SETTINGS_CACHE_SCHEMA: dict[str, Any] = {
     "propertyNames": {"format": "uuid"},  # subscription_id
     "additionalProperties": {
         "type": "object",  # resource_id
-        "additionalProperties": {
-            "oneOf": [
-                EVENT_HUB_DIAGNOSTIC_SETTING_CONFIGURATION_SCHEMA,
-                STORAGE_ACCOUNT_DIAGNOSTIC_SETTING_CONFIGURATION_SCHEMA,
-            ],
-        },
+        "additionalProperties": DIAGNOSTIC_SETTING_CONFIGURATION_SCHEMA,
     },
 }
 
