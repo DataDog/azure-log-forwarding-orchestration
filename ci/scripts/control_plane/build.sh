b2cdddb3d0c637795b752a80284df1cc0bc8f94b
Ava Silver <ava.silver@datadoghq.com>
2024-07-18 15:45:10 -0400

    clean up build script

--- ci/scripts/control_plane/build.sh
+++ ci/scripts/control_plane/build.sh
@@ -13,12 +13,27 @@ cd ..
 for task in $tasks; do
     echo "Building $task"
     mkdir -p ./dist/$task/$task
+
+    : Compile $task into a single file
+    stickytape ./control_plane/tasks/$task.py \
+        --add-python-path ./control_plane \
+        --output-file ./dist/$task/$task/task.py
+
+    : requirements.txt
     # TODO: This is a hack to get the dependencies for the task. We should ideally only pull in the dependencies for the task
-    python -c "import toml; print('\n'.join(toml.load('./control_plane/pyproject.toml')['project']['dependencies']))" > ./dist/$task/requirements.txt
-    stickytape ./control_plane/tasks/$task.py --add-python-path ./control_plane --output-file ./dist/$task/$task/task.py
+    python -c "import toml; print('\n'.join(toml.load('./control_plane/pyproject.toml')['project']['dependencies']))" \
+        > ./dist/$task/requirements.txt
+
+    : entrypoint
     cp ./control_plane/config/__init__.py ./dist/$task/$task/__init__.py
+
+    : function.json
     cp ./control_plane/config/$task/function.json ./dist/$task/$task/function.json
+
+    : host.json
     cp ./control_plane/config/host.json ./dist/$task/host.json
+
+    : zip it up for zipdeploy
     zip ./dist/$task.zip ./dist/$task/*
     echo "Built $task"
 done
