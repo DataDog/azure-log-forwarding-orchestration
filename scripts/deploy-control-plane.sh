2fb5f1a4e68fed145e1b3e2814ae73f048157f02
Ava Silver <ava.silver@datadoghq.com>
2024-07-23 17:20:07 -0400

    update scripts

--- scripts/deploy-control-plane.sh
+++ scripts/deploy-control-plane.sh
@@ -53,7 +53,7 @@ echo Done.
 echo -n "Checking for storage containers on $storage_account..."
 az storage container list --account-name $storage_account --auth-mode login | jq -r '.[].name' | grep $cache_name >/dev/null || {
     echo -n "Missing Cache Container, creating..."
-    az storage container create --name $cache_name --account-name $storage_account
+    az storage container create --name $cache_name --account-name $storage_account --auth-mode login
 }
 echo Done.
 
