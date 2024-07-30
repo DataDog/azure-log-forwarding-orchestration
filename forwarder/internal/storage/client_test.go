fcf7db7ad19204cf569c0978abd95f57581df65d
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-07-30 16:10:51 -0400

    add first test

--- forwarder/internal/storage/client_test.go
+++ forwarder/internal/storage/client_test.go
@@ -2,8 +2,15 @@ package storage_test
 
 import (
 	"context"
+	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/DataDog/azure-log-forwarding-orchestration/forwarder/internal/storage"
+	"github.com/DataDog/azure-log-forwarding-orchestration/forwarder/internal/storage/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 func newPagingHandler[ContentType any, ResponseType any](items []ContentType, fetcherError error, getter func(ContentType) ResponseType, nilValue ResponseType) runtime.PagingHandler[ResponseType] {
@@ -26,4 +33,37 @@ func newPagingHandler[ContentType any, ResponseType any](items []ContentType, fe
 			return counter < len(items)
 		},
 	}
+}
+
+func uploadBuffer(t *testing.T, ctx context.Context, containerName string, blobName string, buffer []byte, expectedResponse azblob.UploadBufferResponse, expectedErr error) error {
+	ctrl := gomock.NewController(t)
+
+	mockClient := mocks.NewMockAzureBlobClient(ctrl)
+	mockClient.EXPECT().UploadBuffer(gomock.Any(), containerName, blobName, buffer, gomock.Any()).Return(expectedResponse, expectedErr)
+
+	client := storage.NewClient(mockClient)
+
+	span, ctx := tracer.StartSpanFromContext(context.Background(), "containers.test")
+	defer span.Finish()
+
+	return client.UploadBuffer(ctx, containerName, blobName, buffer)
+}
+
+func TestUploadBuffer(t *testing.T) {
+	t.Parallel()
+
+	t.Run("uploads a buffer", func(t *testing.T) {
+		t.Parallel()
+		// GIVEN
+		containerName := "container"
+		blobName := "blob"
+		buffer := []byte("data")
+		expectedResponse := azblob.UploadBufferResponse{}
+
+		// WHEN
+		err := uploadBuffer(t, context.Background(), containerName, blobName, buffer, expectedResponse, nil)
+
+		// THEN
+		assert.Nil(t, err)
+	})
 }
