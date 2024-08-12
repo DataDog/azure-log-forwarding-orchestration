7914d25d104c874bcca8600294d704463dc0c62a
Dan-Nedelescu <dan.nedelescu@datadoghq.com>
2024-08-12 15:17:27 -0400

    Initial commit

---
+++ control_plane/cache/manifest_cache.py
@@ -0,0 +1,32 @@
+# stdlib
+from json import JSONDecodeError, loads
+from typing import Any, TypeAlias
+
+# 3p
+from jsonschema import ValidationError, validate
+
+ManifestCache: TypeAlias = dict[str, str]
+"""
+Mapping of deployable name to SHA-256 manifest
+"""
+
+MANIFEST_SCHEMA: dict[str, Any] = {
+    "type": "object",
+    "properties": {
+        "deploy": {"type": "string"},
+        "forwarder": {"type": "string"},
+        "diagnostic_settings": {"type": "string"},
+        "scaling": {"type": "string"},
+    },
+    "required": ["deploy", "forwarder", "diagnostic_settings", "scaling"],
+    "additionalProperties": False,
+}
+
+
+def deserialize_manifest_cache(raw_manifest_cache: str) -> ManifestCache | None:
+    try:
+        manifest_dict: ManifestCache = loads(raw_manifest_cache)
+        validate(instance=manifest_dict, schema=MANIFEST_SCHEMA)
+        return manifest_dict
+    except (JSONDecodeError, ValidationError):
+        return None
---
+++ control_plane/tasks/control_plane_deployer.py
@@ -0,0 +1,82 @@
+# stdlib
+
+from asyncio import gather, run
+from contextlib import AbstractAsyncContextManager
+from logging import DEBUG, INFO, basicConfig, getLogger
+from typing import Self
+
+# 3p
+from tenacity import RetryError, retry, stop_after_attempt
+
+# project
+from cache.manifest_cache import ManifestCache
+
+DEPLOYER_NAME = "control_plane_deployer"
+MAX_ATTEMPTS = 5
+
+log = getLogger(DEPLOYER_NAME)
+log.setLevel(DEBUG)
+
+
+class Deployer(AbstractAsyncContextManager):
+    def __init__(self) -> None:
+        return None
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(self, *_) -> None:
+        pass
+
+    async def run_deployer(self) -> None:
+        public_manifest: dict[str, str] = {}
+        private_manifest: dict[str, str] = {}
+        try:
+            public_manifest, private_manifest = await gather(self.get_public_manifests(), self.get_private_manifests())
+        except RetryError:
+            log.error("Failed to read public manifests, exiting...")
+            return
+        log.info(public_manifest)
+        log.info(private_manifest)
+        if len(public_manifest) == 0:
+            log.error("Failed to read public manifests, exiting...")
+            return
+        try:
+            if len(private_manifest) == 0:
+                await self.deploy_components(list(public_manifest.keys()))
+                pass  # deploy all
+            else:
+                await self.deploy_components(
+                    [
+                        component
+                        for component in public_manifest
+                        if public_manifest[component] != private_manifest[component]
+                    ]
+                )
+                pass
+        except RetryError:
+            log.error("Failed to successfully deploy, exiting...")
+            return
+        return
+
+    @retry(stop=stop_after_attempt(MAX_ATTEMPTS))
+    async def get_public_manifests(self) -> ManifestCache:
+        return {"test": "hi"}
+
+    async def get_private_manifests(self) -> ManifestCache:
+        return {"test": "hi2"}
+
+    @retry(stop=stop_after_attempt(MAX_ATTEMPTS))
+    async def deploy_components(self, component_names: list[str]) -> None:
+        log.info(component_names)
+        pass
+
+
+async def main():
+    async with Deployer() as deployer:
+        await deployer.run_deployer()
+
+
+if __name__ == "__main__":
+    basicConfig(level=INFO)
+    run(main())
