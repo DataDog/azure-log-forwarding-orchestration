11b88674af27e4edfc90c67a5754dfb598773ea3
Ava Silver <ava.silver@datadoghq.com>
2024-08-13 17:56:38 -0400

    [AZINTS-2687] fetch api versions beforehand

--- control_plane/tasks/resources_task.py
+++ control_plane/tasks/resources_task.py
@@ -1,22 +1,28 @@
 # stdlib
-from asyncio import gather, run
+from asyncio import create_task, gather, run
 from json import dumps
 from logging import DEBUG, basicConfig, getLogger
 from typing import cast
 
 # 3p
 from azure.mgmt.resource.resources.v2021_01_01.aio import ResourceManagementClient
+from azure.mgmt.resource.resources.v2021_01_01.models import ProviderResourceType
 from azure.mgmt.resource.subscriptions.v2021_01_01.aio import SubscriptionClient
 
 # project
 from cache.common import read_cache, write_cache
 from cache.resources_cache import RESOURCE_CACHE_BLOB, ResourceCache, deserialize_resource_cache
+from tasks.client.log_forwarder_client import ignore_exception_type
 from tasks.common import now
 from tasks.task import Task
 
 RESOURCES_TASK_NAME = "resources_task"
 
 log = getLogger(RESOURCES_TASK_NAME)
+
+
+def get_resource_type(resource_id: str) -> str:
+    return resource_id.split("/")[6]
 
 
 class ResourcesTask(Task):
@@ -43,6 +49,7 @@ class ResourcesTask(Task):
     async def process_subscription(self, subscription_id: str) -> None:
         log.debug("Processing the following subscription: %s", subscription_id)
         async with ResourceManagementClient(self.credential, subscription_id) as client:
+            api_versions_task = create_task(self.fetch_api_versions(client))
             resources_per_region: dict[str, set[str]] = {}
             resource_count = 0
             async for r in client.resources.list():
@@ -53,29 +60,60 @@ class ResourcesTask(Task):
                 resource_count += 1
             log.debug("Subscription %s: Collected %s resources", subscription_id, resource_count)
             self.resource_cache[subscription_id] = resources_per_region
-            await self.confirm_deletions_for_subscription(client, subscription_id)
+            api_versions = await api_versions_task
+            await self.confirm_deletions_for_subscription(client, subscription_id, api_versions)
 
-    async def confirm_deletions_for_subscription(self, client: ResourceManagementClient, subscription_id: str) -> None:
+    async def fetch_api_versions(self, client: ResourceManagementClient) -> dict[str, str]:
+        """Fetch all the api versions by resource type"""
+        return {
+            cast(str, resource_type.resource_type): api_version
+            async for provider in client.providers.list()
+            for resource_type in cast(list[ProviderResourceType], provider.resource_types)
+            if (api_version := resource_type.api_versions[0] if resource_type.api_versions else None)
+        }
+
+    async def confirm_deletions_for_subscription(
+        self, client: ResourceManagementClient, subscription_id: str, api_versions: dict[str, str]
+    ) -> None:
         if subscription_id not in self._resource_cache_initial_state:
             # we discovered a new subscription, no need to check for deletions
             return
 
         await gather(
             *(
-                self.confirm_deletion(
-                    client,
-                    subscription_id,
-                    region,
-                    previous_resources - self.resource_cache.get(subscription_id, {}).get(region, set()),
-                )
+                self.confirm_deletion(client, subscription_id, region, deleted_resources, api_versions)
                 for region, previous_resources in self.resource_cache.get(subscription_id, {}).items()
+                if (
+                    deleted_resources := previous_resources
+                    - self.resource_cache.get(subscription_id, {}).get(region, set())
+                )
             )
         )
 
     async def confirm_deletion(
-        self, client: ResourceManagementClient, subscription_id: str, region: str, resources: set[str]
+        self,
+        client: ResourceManagementClient,
+        subscription_id: str,
+        region: str,
+        resources: set[str],
+        api_versions: dict[str, str],
     ) -> None:
         """Confirm that the resources are deleted before removing them from the cache"""
+        fetched_resources = await gather(
+            *(
+                ignore_exception_type(Exception, client.resources.get_by_id(resource_id, api_version))
+                for resource_id in resources
+                if (api_version := api_versions.get(get_resource_type(resource_id)))
+            )
+        )
+        for resource_id, fetched_resource in zip(resources, fetched_resources, strict=False):
+            if fetched_resource is not None:
+                log.debug(
+                    "Subscription %s: Resource %s was not found in the list endpoint, but still exists. Keeping in the cache",
+                    subscription_id,
+                    resource_id,
+                )
+                self.resource_cache[subscription_id][region].add(resource_id)
 
     async def write_caches(self) -> None:
         if self.resource_cache == self._resource_cache_initial_state:
