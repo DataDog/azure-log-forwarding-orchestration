35e5e480d93aae609221df8d81bb511e1e8baee5
Dan-Nedelescu <dan.nedelescu@datadoghq.com>
2024-08-14 16:12:25 -0400

    Fill out methods

--- control_plane/tasks/deployer_task.py
+++ control_plane/tasks/deployer_task.py
@@ -4,19 +4,23 @@ from asyncio import gather, run
 from copy import deepcopy
 from json import dumps
 from logging import DEBUG, INFO, basicConfig, getLogger
+from types import TracebackType
 from typing import Self
 
 # 3p
+from aiohttp import ClientSession
+from azure.storage.blob.aio import ContainerClient
 from tenacity import RetryError, retry, stop_after_attempt
 
-from cache.common import write_cache
-
 # project
-from cache.manifest_cache import MANIFEST_CACHE_NAME, ManifestCache
+from cache.common import read_cache, write_cache
+from cache.manifest_cache import MANIFEST_CACHE_NAME, ManifestCache, deserialize_manifest_cache
 from tasks.task import Task
 
 DEPLOYER_NAME = "control_plane_deployer"
 MAX_ATTEMPTS = 5
+
+PUBLIC_CONTAINER_URL = "google.com"
 
 log = getLogger(DEPLOYER_NAME)
 log.setLevel(DEBUG)
@@ -26,13 +30,25 @@ class Deployer(Task):
     def __init__(self) -> None:
         self.manifest_cache: ManifestCache = {}
         self.original_manifest_cache: ManifestCache = {}
+        self.public_manifest = {}
+        self.public_client = ContainerClient.from_container_url(PUBLIC_CONTAINER_URL)
+        self.rest_client = ClientSession()
         super().__init__()
         return
 
     async def __aenter__(self) -> Self:
-        return await super().__aenter__()
+        await super().__aenter__()
+        await self.public_client.__aenter__()
+        await self.rest_client.__aenter__()
+        token = await self.credential.get_token("https://management.azure.com/.default")
+        self.rest_client.headers["Authorization"] = f"Bearer {token.token}"
+        return self
 
-    async def __aexit__(self, *_) -> None:
+    async def __aexit__(
+        self, exc_type: type[BaseException] | None, exc_val: BaseException | None, exc_tb: TracebackType | None
+    ) -> None:
+        await self.public_client.__aexit__(exc_type, exc_val, exc_tb)
+        await self.rest_client.__aexit__(exc_type, exc_val, exc_tb)
         await super().__aexit__()
 
     async def run(self) -> None:
@@ -43,13 +59,12 @@ class Deployer(Task):
         except RetryError:
             log.error("Failed to read public manifests, exiting...")
             return
-        log.info(public_manifest)
-        log.info(private_manifest)
         if len(public_manifest) == 0:
             log.error("Failed to read public manifests, exiting...")
             return
         self.manifest_cache = deepcopy(private_manifest)
         self.original_manifest_cache = private_manifest
+        self.public_manifest = public_manifest
         try:
             if len(private_manifest) == 0:
                 await self.deploy_components(list(public_manifest.keys()))  # deploy all
@@ -71,17 +86,46 @@ class Deployer(Task):
     async def get_public_manifests(self) -> ManifestCache:
         return {"test": "hi"}
 
+    @retry(stop=stop_after_attempt(MAX_ATTEMPTS))
     async def get_private_manifests(self) -> ManifestCache:
-        return {"test": "hi2"}
+        blob_data = await read_cache(MANIFEST_CACHE_NAME)
+        validated_blob = deserialize_manifest_cache(blob_data)
+        if validated_blob:
+            return validated_blob
+        return {}
 
     @retry(stop=stop_after_attempt(MAX_ATTEMPTS))
     async def deploy_components(self, component_names: list[str]) -> None:
         log.info(component_names)
         pass
 
-    @retry(stop=stop_after_attempt(MAX_ATTEMPTS))
     async def deploy_function_apps(self, function_app_names: list[str]) -> None:
+        try:
+            await gather(*[self.deploy_function_app(function_app_name) for function_app_name in function_app_names])
+        except RetryError:
+            log.error("Error deploy function apps")
+        return
+
+    @retry(stop=stop_after_attempt(MAX_ATTEMPTS))
+    async def deploy_container_apps(self, container_app_names: list[str]) -> None:
         pass
+
+    @retry(stop=stop_after_attempt(MAX_ATTEMPTS))
+    async def deploy_function_app(self, function_app_name: str) -> None:
+        function_app_data = await self.download_function_app_data(function_app_name)
+        resp = await self.rest_client.post(
+            f"https://{function_app_name}.scm.azurewebsites.net/api/publish?type=zip",  # TODO ASK HERE
+            data=function_app_data,
+        )
+        resp.raise_for_status()
+        self.manifest_cache[function_app_name] = self.public_manifest[function_app_name]
+
+    @retry(stop=stop_after_attempt(MAX_ATTEMPTS))
+    async def download_function_app_data(self, function_app_name: str):
+        blob_name = function_app_name + "_task.zip"
+        stream = await self.public_client.download_blob(blob_name)
+        app_data = await stream.content_as_bytes(max_concurrency=4)
+        return app_data
 
     async def write_caches(self) -> None:
         if self.manifest_cache == self.original_manifest_cache:
