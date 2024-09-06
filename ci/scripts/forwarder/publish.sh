a059d688729748e2aaabb8720db28762926566a5
Matt Spurlin <matt.spurlin@datadoghq.com>
2024-09-06 18:02:08 -0400

    add tag

--- ci/scripts/forwarder/publish.sh
+++ ci/scripts/forwarder/publish.sh
@@ -7,4 +7,4 @@ cd forwarder
 #docker build --tag 486234852809.dkr.ecr.us-east-1.amazonaws.com/lfo/forwarder:latest .
 #docker push 486234852809.dkr.ecr.us-east-1.amazonaws.com/lfo/forwarder:latest
 #docker tag 486234852809.dkr.ecr.us-east-1.amazonaws.com/lfo/forwarder:latest registry.ddbuild.io/lfo/forwarder:latest
-docker buildx build --platform=linux/amd64 --label target=build registry.ddbuild.io/lfo/forwarder:latest --push
+docker buildx build --platform=linux/amd64 --label target=build --tag registry.ddbuild.io/lfo/forwarder:latest --push
