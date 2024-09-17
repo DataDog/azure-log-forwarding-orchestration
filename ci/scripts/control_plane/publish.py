b8a9a408da8fc9a3977f5587e8f3c05115bae42d
Ava Silver <ava.silver@datadoghq.com>
2024-09-17 17:42:02 -0400

    [AZINTS] try this

--- ci/scripts/control_plane/publish.py
+++ ci/scripts/control_plane/publish.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
-
+# stdlib
 from concurrent.futures import ThreadPoolExecutor
+from os import listdir
+from os.path import isfile
+
+# 3p
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import ContainerClient
-from tasks.__main__ import TASKS
 
 ACCOUNT_URL = "https://ddazurelfo.blob.core.windows.net"
 TASKS_CONTAINER = "tasks"
 
+zips = [f for f in listdir("dist") if isfile(f) and f.endswith(".zip")]
 
-for task in TASKS:
-    print(task)
-
+print(zips)
 raise SystemExit(0)
 
 with DefaultAzureCredential() as cred, ContainerClient(
