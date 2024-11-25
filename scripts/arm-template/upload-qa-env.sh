4cdfd6f9957978ab02af8467d5a2f2e0fdfcd89a
Altan Gulen <altan.gulen@datadoghq.com>
2024-11-25 14:38:09 -0500

    fix script, dont need export

--- scripts/arm-template/upload-qa-env.sh
+++ scripts/arm-template/upload-qa-env.sh
@@ -6,9 +6,9 @@ AZURE_TENANT_ID=$(vault kv get -field=azureTenantId kv/k8s/gitlab-runner/azure-l
 AZURE_CLIENT_ID=$(vault kv get -field=azureClientId kv/k8s/gitlab-runner/azure-log-forwarding-orchestration/qa)
 AZURE_CLIENT_SECRET=$(vault kv get -field=azureSecret kv/k8s/gitlab-runner/azure-log-forwarding-orchestration/qa)
 
-az login --identity --username "$AZURE_CLIENT_ID"
+az login --identity --username $AZURE_CLIENT_ID
 
-export connection=$(az storage account show-connection-string --resource-group lfo-qa --name lfoqa --query connectionString)
+connection=$(az storage account show-connection-string --resource-group lfo-qa --name lfoqa --query connectionString)
 
 az storage blob upload --container-name templates --file ./createUiDefinition.json --name createUiDefinition.json --connection-string $connection --overwrite
 az storage blob upload --container-name templates --file ./build/azuredeploy.json --name azuredeploy.json --connection-string $connection --overwrite
