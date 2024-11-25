b47ef547429b4e3a7b23f4a0e7df2df541b61531
Altan Gulen <altan.gulen@datadoghq.com>
2024-11-25 13:58:08 -0500

    remove cd from upload

--- scripts/arm-template/upload.sh
+++ scripts/arm-template/upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-cd $HOME/dd/azure-log-forwarding-orchestration/deploy
+# cd $HOME/dd/azure-log-forwarding-orchestration/deploy #altan - won't work in CI. you'll be in lfo folder already. cd into deploy
 
 az storage blob upload --container-name templates --file ./createUiDefinition.json --name createUiDefinition.json --connection-string $connection --overwrite
 
