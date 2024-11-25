3ff9b2d9b583483c908caf275cfcc4ce9d80dd5c
Altan Gulen <altan.gulen@datadoghq.com>
2024-11-25 14:36:18 -0500

    try identity login

--- scripts/arm-template/upload-qa-env.sh
+++ scripts/arm-template/upload-qa-env.sh
@@ -6,7 +6,7 @@ AZURE_TENANT_ID=$(vault kv get -field=azureTenantId kv/k8s/gitlab-runner/azure-l
 AZURE_CLIENT_ID=$(vault kv get -field=azureClientId kv/k8s/gitlab-runner/azure-log-forwarding-orchestration/qa)
 AZURE_CLIENT_SECRET=$(vault kv get -field=azureSecret kv/k8s/gitlab-runner/azure-log-forwarding-orchestration/qa)
 
-az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" --tenant "$AZURE_TENANT_ID"
+az login --identity --username "$AZURE_CLIENT_ID"
 
 export connection=$(az storage account show-connection-string --resource-group lfo-qa --name lfoqa --query connectionString)
 
