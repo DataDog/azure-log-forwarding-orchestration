c8a1c62debc548cbf421def8e8480dfebdcb2bdc
Ava Silver <ava.silver@datadoghq.com>
2025-01-17 12:34:44 -0500

    [AZINTS-2763] take a stab at getting more resource types

--- control_plane/tasks/client/resource_client.py
+++ control_plane/tasks/client/resource_client.py
@@ -1,14 +1,18 @@
 # stdlib
-from collections.abc import AsyncGenerator, AsyncIterable, Callable, Mapping
+from asyncio import gather
+from collections.abc import AsyncGenerator, AsyncIterable, Callable, Iterable, Mapping
 from contextlib import AbstractAsyncContextManager
 from logging import getLogger
 from types import TracebackType
-from typing import Final, Self, TypeAlias, cast
+from typing import Any, Final, Self, TypeAlias, cast
 
 # 3p
 from azure.identity.aio import DefaultAzureCredential
+from azure.mgmt.core.tools import parse_resource_id
 from azure.mgmt.resource.resources.v2021_01_01.aio import ResourceManagementClient
 from azure.mgmt.resource.resources.v2021_01_01.models import GenericResourceExpanded
+from azure.mgmt.sql.aio import SqlManagementClient
+from azure.mgmt.web.v2023_12_01.aio import WebSiteManagementClient
 
 # project
 from tasks.common import (
@@ -52,7 +56,32 @@ async def get_storage_account_services(r: GenericResourceExpanded) -> AsyncGener
         yield f"{r.id}/{service_type}/default"
 
 
-async def make_sub_resource_extractor_for_name_and_rg() -> FetchSubResources: ...
+def safe_get_id(r: Any) -> str | None:
+    if hasattr(r, "id") and isinstance(r.id, str):
+        return r.id.lower()
+    return None
+
+
+def make_sub_resource_extractor_for_rg_and_name(f: Callable[[str, str], AsyncIterable[Any]]) -> FetchSubResources:
+    """Creates an extractor for sub resource IDs based on the resource group and name"""
+
+    async def _f(r: GenericResourceExpanded) -> AsyncGenerator[str]:
+        resource_group = getattr(r, "resource_group", None)
+        resource_name = r.name
+        if not (resource_group and resource_name):  # fallback to parsing the resource id
+            parsed = parse_resource_id(r.id)
+            resource_group = parsed["resource_group"]
+            resource_name = parsed["name"]
+
+        async for sub_resource in f(resource_group, resource_name):
+            if hasattr(sub_resource, "value") and isinstance(sub_resource.value, Iterable):
+                for resource in sub_resource.value:
+                    if rid := safe_get_id(resource):
+                        yield rid
+            if rid := safe_get_id(sub_resource):
+                yield rid
+
+    return _f
 
 
 def should_ignore_resource(region: str, resource_type: str, resource_name: str) -> bool:
@@ -75,36 +104,62 @@ class ResourceClient(AbstractAsyncContextManager["ResourceClient"]):
         self.credential = cred
         self.subscription_id = subscription_id
         self.resources_client = ResourceManagementClient(cred, subscription_id)
-
-        self._get_sub_resources_map: Final[Mapping[str, FetchSubResources | None]] = {
-            "microsoft.azuredatatransfer/connections": None,  # {"flows"},
-            "microsoft.cache/redisenterprise": None,  # {"databases"},
-            "microsoft.cdn/profiles": None,  # {"endpoints"},
-            "microsoft.healthcareapis/workspaces": None,  # {"dicomservices", "fhirservices", "iotconnectors"},
-            "microsoft.machinelearningservices/workspaces": None,  # {"onlineendpoints"},
-            "microsoft.media/mediaservices": None,  # {"liveevents", "streamingendpoints"},
-            "microsoft.netapp/netappaccounts": None,  # { "capacitypools","capacitypools/volumes",  # this is also a doubly nested subtype but i dont wanna deal with that rn},
-            "microsoft.network/networksecurityperimeters": None,  # {"profiles"},
-            "microsoft.network/networkmanagers": None,  # {"ipampools"},
-            "microsoft.notificationhubs/namespaces": None,  # {"notificationhubs"},
-            "microsoft.powerbi/tenants": None,  # {"workspaces"},
-            "microsoft.signalrservice/signalr": None,  # {"replicas"},
-            "microsoft.signalrservice/webpubsub": None,  # {"replicas"},
-            "microsoft.storage/storageaccounts": get_storage_account_services,  # {"blobservices", "fileservices", "queueservices", "tableservices"},
-            "microsoft.sql/servers": None,  # {"databases"},
-            "microsoft.sql/managedinstances": None,  # {"databases"},
-            "microsoft.synapse/workspaces": None,  # {"bigdatapools", "kustopools", "scopepools", "sqlpools"},
-            "microsoft.web/sites": None,  # {"slots"},
+        web_client = WebSiteManagementClient(cred, subscription_id)
+        sql_client = SqlManagementClient(cred, subscription_id)
+        # map of resource type to client and sub resource fetching function
+        self._get_sub_resources_map: Final[
+            Mapping[str, tuple[AbstractAsyncContextManager | None, FetchSubResources | None]]
+        ] = {
+            "microsoft.azuredatatransfer/connections": (None, None),  # {"flows"},
+            "microsoft.cache/redisenterprise": (None, None),  # {"databases"},
+            "microsoft.cdn/profiles": (None, None),  # {"endpoints"},
+            "microsoft.healthcareapis/workspaces": (None, None),  # {"dicomservices", "fhirservices", "iotconnectors"},
+            "microsoft.machinelearningservices/workspaces": (None, None),  # {"onlineendpoints"},
+            "microsoft.media/mediaservices": (None, None),  # {"liveevents", "streamingendpoints"},
+            "microsoft.netapp/netappaccounts": (
+                None,
+                None,
+            ),  # { "capacitypools","capacitypools/volumes",  # this is also a doubly nested subtype but i dont wanna deal with that rn},
+            "microsoft.network/networksecurityperimeters": (None, None),  # {"profiles"},
+            "microsoft.network/networkmanagers": (None, None),  # {"ipampools"},
+            "microsoft.notificationhubs/namespaces": (None, None),  # {"notificationhubs"},
+            "microsoft.powerbi/tenants": (None, None),  # {"workspaces"},
+            "microsoft.signalrservice/signalr": (None, None),  # {"replicas"},
+            "microsoft.signalrservice/webpubsub": (None, None),  # {"replicas"},
+            "microsoft.storage/storageaccounts": (None, get_storage_account_services),
+            "microsoft.sql/servers": (
+                sql_client,
+                make_sub_resource_extractor_for_rg_and_name(sql_client.databases.list_by_server),
+            ),  # {"databases"},
+            "microsoft.sql/managedinstances": (
+                None,
+                make_sub_resource_extractor_for_rg_and_name(sql_client.managed_databases.list_by_instance),
+            ),  # {"databases"},
+            "microsoft.synapse/workspaces": (None, None),  # {"bigdatapools", "kustopools", "scopepools", "sqlpools"},
+            "microsoft.web/sites": (
+                web_client,
+                make_sub_resource_extractor_for_rg_and_name(web_client.web_apps.list_slots),
+            ),
         }
 
     async def __aenter__(self) -> Self:
-        await self.resources_client.__aenter__()
+        await gather(
+            self.resources_client.__aenter__(),
+            *(client.__aenter__() for client, _ in self._get_sub_resources_map.values() if client is not None),
+        )
         return self
 
     async def __aexit__(
         self, exc_type: type[BaseException] | None, exc_val: BaseException | None, exc_tb: TracebackType | None
     ) -> None:
-        await self.resources_client.__aexit__(exc_type, exc_val, exc_tb)
+        await gather(
+            self.resources_client.__aexit__(exc_type, exc_val, exc_tb),
+            *(
+                client.__aexit__(exc_type, exc_val, exc_tb)
+                for client, _ in self._get_sub_resources_map.values()
+                if client is not None
+            ),
+        )
 
     async def get_resources_per_region(self) -> dict[str, set[str]]:
         resources_per_region: dict[str, set[str]] = {}
@@ -125,6 +180,8 @@ class ResourceClient(AbstractAsyncContextManager["ResourceClient"]):
         resource_type = cast(str, resource.type).lower()
         if resource_type in NON_NESTED_VALID_RESOURCE_TYPES:
             yield resource_id
-        if get_sub_resources := self._get_sub_resources_map.get(resource_type):
+        if resource_type in self._get_sub_resources_map and (
+            get_sub_resources := self._get_sub_resources_map[resource_type][1]
+        ):
             async for sub_resource in get_sub_resources(resource):
                 yield sub_resource
