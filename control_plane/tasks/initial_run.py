a1d5769c727d9e58d9e7ba758255ed4e3df4646c
Ava Silver <ava.silver@datadoghq.com>
2025-02-12 17:04:38 -0500

    [AZINTS-3119] add initial run script

--- control_plane/tasks/initial_run.py
+++ control_plane/tasks/initial_run.py
@@ -1,7 +1,27 @@
-from tasks.diagnostic_settings_task import DIAGNOSTIC_SETTINGS_TASK_NAME
-from tasks.resources_task import RESOURCES_TASK_NAME
-from tasks.scaling_task import SCALING_TASK_NAME
+# stdlib
+from asyncio import run
+from json import dumps
 
-for task in (RESOURCES_TASK_NAME, DIAGNOSTIC_SETTINGS_TASK_NAME, SCALING_TASK_NAME):
-    print(f"hello {task}")
+# project
+from tasks.diagnostic_settings_task import DiagnosticSettingsTask
+from tasks.resources_task import ResourcesTask
+from tasks.scaling_task import ScalingTask
+
+
+async def main() -> None:
+    async with ResourcesTask("") as resources_task:
+        await resources_task.run()
+        resource_cache = dumps(resources_task.resource_cache, default=list)
+    async with ScalingTask("", resource_cache, wait=True) as scaling_task:
+        await scaling_task.run()
+        assignment_cache = dumps(scaling_task.assignment_cache)
+    async with ScalingTask(assignment_cache, resource_cache) as scaling_task:
+        await scaling_task.run()
+        assignment_cache = dumps(scaling_task.assignment_cache)
+    async with DiagnosticSettingsTask(assignment_cache) as diagnostic_settings_task:
+        await diagnostic_settings_task.run()
+
+
+run(main())
+
 # Start-AzContainerAppJob -Name ${deployerTaskName} -ResourceGroupName ${controlPlaneResourceGroupName}
