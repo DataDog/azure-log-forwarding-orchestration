c30f360a8a5796ff2bb0350c17586754b880fd14
Ava Silver <ava.silver@datadoghq.com>
2025-02-26 13:48:27 -0500

    [AZINTS] move scripts to scripts/

--- control_plane/scripts/publish.py
+++ control_plane/scripts/publish.py
@@ -2,24 +2,24 @@
 # usage: publish.py <public_storage_account_url> [connection_string]
 
 # stdlib
+import sys
 from concurrent.futures import ThreadPoolExecutor
 from hashlib import sha256
 from json import dumps
-from logging import WARNING, getLogger, basicConfig, INFO
-import sys
+from logging import INFO, WARNING, basicConfig, getLogger
 
 # 3p
 from azure.identity import DefaultAzureCredential
-from azure.storage.blob import ContainerClient, BlobServiceClient
+from azure.storage.blob import BlobServiceClient, ContainerClient
 
 from cache.manifest_cache import (
-    ManifestCache,
     ALL_ZIPS,
-    RESOURCES_TASK_ZIP,
-    SCALING_TASK_ZIP,
     DIAGNOSTIC_SETTINGS_TASK_ZIP,
     MANIFEST_FILE_NAME,
+    RESOURCES_TASK_ZIP,
+    SCALING_TASK_ZIP,
     TASKS_CONTAINER,
+    ManifestCache,
 )
 
 if len(sys.argv) < 2:
@@ -68,15 +68,8 @@ with (
     if not client.exists():
         log.warning("Container %s does not exist, creating it...", TASKS_CONTAINER)
         client.create_container(public_access="container")
-    futures = [
-        executor.submit(client.upload_blob, zip, data, overwrite=True)
-        for zip, data in zips.items()
-    ]
-    futures.append(
-        executor.submit(
-            client.upload_blob, MANIFEST_FILE_NAME, dumps(hashes), overwrite=True
-        )
-    )
+    futures = [executor.submit(client.upload_blob, zip, data, overwrite=True) for zip, data in zips.items()]
+    futures.append(executor.submit(client.upload_blob, MANIFEST_FILE_NAME, dumps(hashes), overwrite=True))
     exceptions = [e for f in futures if (e := f.exception())]
     for e in exceptions:
         log.error("An error occurred while uploading a zip file", exc_info=e)
