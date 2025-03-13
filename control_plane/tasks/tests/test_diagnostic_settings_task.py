2a995bd71f064dd4439e1cf4270f424ead4cd6e4
Altan Gulen <altan.gulen@datadoghq.com>
2025-03-13 18:54:34 -0400

    diag setting tests

--- control_plane/tasks/tests/test_diagnostic_settings_task.py
+++ control_plane/tasks/tests/test_diagnostic_settings_task.py
@@ -2,7 +2,7 @@
 from collections.abc import AsyncIterable
 from json import dumps
 from os import environ
-from typing import Final
+from typing import Final, cast
 from unittest.mock import AsyncMock, Mock, patch
 
 # 3p
@@ -29,7 +29,10 @@ config_id1: Final = "bc666ef914ec"
 control_plane_id: Final = "e90ecb54476d"
 DIAGNOSTIC_SETTING_NAME: Final = DIAGNOSTIC_SETTING_PREFIX + control_plane_id
 resource_id1: Final = "/subscriptions/1/resourcegroups/rg1/providers/microsoft.compute/virtualmachines/vm1"
-storage_account1: Final = "/subscriptions/1/resourceGroups/lfo/providers/Microsoft.Storage/storageAccounts/storageacc1"
+resource_id2: Final = "/subscriptions/1/resourcegroups/rg1/providers/microsoft.compute/virtualmachines/vm2"
+storage_account: Final = (
+    f"/subscriptions/{sub_id1}/resourcegroups/lfo/providers/microsoft.storage/storageaccounts/ddlogstorage{config_id1}"
+)
 
 
 def mock_diagnostic_settings(count: int) -> AsyncIterable[Mock]:
@@ -48,6 +51,7 @@ class TestDiagnosticSettingsTask(TaskTestCase):
         client.diagnostic_settings_category.list = Mock()
         self.list_diagnostic_settings_categories: Mock = client.diagnostic_settings_category.list
         self.create_or_update_setting: AsyncMock = client.diagnostic_settings.create_or_update
+        self.delete_diagnostic_setting: AsyncMock = client.diagnostic_settings.delete
         client.subscription_diagnostic_settings.list = Mock(return_value=async_generator())  # nothing to test here yet
 
         self.send_max_settings_reached_event = self.patch("DiagnosticSettingsTask.send_max_settings_reached_event")
@@ -63,11 +67,13 @@ class TestDiagnosticSettingsTask(TaskTestCase):
         assignment_cache: AssignmentCache,
         event_cache: DiagnosticSettingsCache | None,
     ) -> DiagnosticSettingsTask:
-        async with DiagnosticSettingsTask(dumps(resource_cache), dumps(assignment_cache), dumps(event_cache)) as task:
+        async with DiagnosticSettingsTask(
+            dumps(resource_cache, default=list), dumps(assignment_cache), dumps(event_cache)
+        ) as task:
             await task.run()
         return task
 
-    def test_malformed_resources_cache_errors_in_constructor(self):
+    def test_malformed_assignment_cache_errors_in_constructor(self):
         with self.assertRaises(InvalidCacheError) as e:
             DiagnosticSettingsTask("", "malformed", "")
         self.assertEqual(
@@ -124,6 +130,7 @@ class TestDiagnosticSettingsTask(TaskTestCase):
             event_cache=None,
         )
         self.create_or_update_setting.assert_not_awaited()
+        self.delete_diagnostic_setting.assert_not_awaited()
 
     async def test_task_updates_incorrect_settings(self):
         self.list_diagnostic_settings.return_value = async_generator(
@@ -354,3 +361,126 @@ class TestDiagnosticSettingsTask(TaskTestCase):
         self.send_max_settings_reached_event.assert_called_once_with(sub_id1, resource_id1)
         self.assertEqual(task.event_cache[sub_id1][resource_id1][DIAGNOSTIC_SETTINGS_COUNT], MAX_DIAGNOSTIC_SETTINGS)
         self.assertFalse(task.event_cache[sub_id1][resource_id1][SENT_EVENT])
+
+    async def test_latest_resources_cache_schema_creates_setting(self):
+        self.list_diagnostic_settings.return_value = async_generator()
+        self.list_diagnostic_settings_categories.return_value = async_generator(
+            mock(name="cool_logs", category_type=CategoryType.LOGS)
+        )
+
+        await self.run_diagnostic_settings_task(
+            resource_cache={
+                sub_id1: {
+                    region1: {
+                        resource_id1: {"tags": ["filter:me"], "filtered_out": False},
+                    }
+                },
+            },
+            assignment_cache={
+                sub_id1: {
+                    region1: {
+                        "configurations": {config_id1: STORAGE_ACCOUNT_TYPE},
+                        "resources": {resource_id1: config_id1},
+                    }
+                }
+            },
+            event_cache={},
+        )
+
+        self.create_or_update_setting.assert_awaited_once_with(
+            resource_id1,
+            DIAGNOSTIC_SETTING_NAME,
+            AzureModelMatcher(
+                {
+                    "logs": [{"category": "cool_logs", "enabled": True}],
+                    "storage_account_id": "/subscriptions/sub1/resourcegroups/lfo/providers/microsoft.storage/storageaccounts/ddlogstoragebc666ef914ec",
+                }
+            ),
+        )
+
+    async def test_old_resources_cache_schema_creates_setting(self):
+        self.list_diagnostic_settings.return_value = async_generator()
+        self.list_diagnostic_settings_categories.return_value = async_generator(
+            mock(name="cool_logs", category_type=CategoryType.LOGS)
+        )
+
+        resource_cache = {
+            sub_id1: {region1: {resource_id1}},
+        }
+        await self.run_diagnostic_settings_task(
+            resource_cache=cast(ResourceCache, resource_cache),
+            assignment_cache={
+                sub_id1: {
+                    region1: {
+                        "configurations": {config_id1: STORAGE_ACCOUNT_TYPE},
+                        "resources": {resource_id1: config_id1},
+                    }
+                }
+            },
+            event_cache={},
+        )
+
+        self.create_or_update_setting.assert_awaited_once_with(
+            resource_id1,
+            DIAGNOSTIC_SETTING_NAME,
+            AzureModelMatcher(
+                {
+                    "logs": [{"category": "cool_logs", "enabled": True}],
+                    "storage_account_id": "/subscriptions/sub1/resourcegroups/lfo/providers/microsoft.storage/storageaccounts/ddlogstoragebc666ef914ec",
+                }
+            ),
+        )
+
+    async def test_filtered_resource_skips_setting_create(self):
+        self.list_diagnostic_settings.return_value = async_generator()
+        self.list_diagnostic_settings_categories.return_value = async_generator(
+            mock(name="cool_logs", category_type=CategoryType.LOGS)
+        )
+
+        await self.run_diagnostic_settings_task(
+            resource_cache={
+                sub_id1: {
+                    region1: {
+                        resource_id1: {"tags": ["filter:me"], "filtered_out": True},
+                    }
+                },
+            },
+            assignment_cache={
+                sub_id1: {
+                    region1: {
+                        "configurations": {config_id1: STORAGE_ACCOUNT_TYPE},
+                        "resources": {resource_id1: config_id1},
+                    }
+                }
+            },
+            event_cache={},
+        )
+
+        self.create_or_update_setting.assert_not_awaited()
+
+    async def test_filtered_resource_deletes_existing_setting(self):
+        self.list_diagnostic_settings.return_value = async_generator(
+            mock(name=DIAGNOSTIC_SETTING_NAME, storage_account_id=storage_account)
+        )
+
+        await self.run_diagnostic_settings_task(
+            resource_cache={
+                sub_id1: {
+                    region1: {
+                        resource_id1: {"tags": ["filter:me"], "filtered_out": True},
+                    }
+                },
+            },
+            assignment_cache={
+                sub_id1: {
+                    region1: {
+                        "configurations": {config_id1: STORAGE_ACCOUNT_TYPE},
+                        "resources": {resource_id1: config_id1},
+                    }
+                }
+            },
+            event_cache=None,
+        )
+
+        self.create_or_update_setting.assert_not_awaited()
+        self.delete_diagnostic_setting.assert_awaited_once_with(resource_id1, DIAGNOSTIC_SETTING_NAME)
