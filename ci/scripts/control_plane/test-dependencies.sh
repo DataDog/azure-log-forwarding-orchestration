76c3e7da352e78651245de720371940367669cea
Ava Silver <ava.silver@datadoghq.com>
2025-03-14 13:06:24 -0400

    [AZINTS] switch to quiet and use uv from build image

--- ci/scripts/control_plane/test-dependencies.sh
+++ ci/scripts/control_plane/test-dependencies.sh
@@ -3,16 +3,15 @@
 set -euo pipefail
 
 cd ./control_plane
-/venv/bin/pip install uv
 
 test_task() {
     local task_name=$1
     echo "Testing $task_name. If there is an import error," \
         "the task is missing a dependency or is importing something incorrectly."
-    /venv/bin/uv venv ./test_venv
+    /venv/bin/uv --quiet venv ./test_venv
     source ./test_venv/bin/activate
 
-    /venv/bin/uv pip install ".[$task_name]" >/dev/null
+    /venv/bin/uv --quiet pip install ".[$task_name]"
 
     task_name_const="${task_name^^}_NAME"
     python -c "from tasks.$task_name import $task_name_const; print($task_name_const, 'successfully imported')"
