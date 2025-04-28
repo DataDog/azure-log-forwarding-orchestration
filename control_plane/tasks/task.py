9e74ba06187a7893c93b1fc29f89ce8465a2c7e9
Ava Silver <ava.silver@datadoghq.com>
2025-04-28 17:30:23 -0400

    [AZINTS] move out of type ignore

--- control_plane/tasks/task.py
+++ control_plane/tasks/task.py
@@ -114,34 +114,40 @@ class Task(AbstractAsyncContextManager["Task"]):
     async def submit_telemetry(self) -> None:
         if not self.telemetry_enabled or not self._logs:
             return
-        dd_logs = [
-            HTTPLogItem(
-                **{
-                    **{k: str(v) for k, v in record.__dict__.items() if k.lower() not in IGNORED_LOG_EXTRAS},
+        dd_logs = HTTPLog(
+            value=[
+                HTTPLogItem(
                     **{
-                        "message": record.getMessage(),
-                        "ddsource": "azure",
-                        "service": "lfo",
-                        "time": record.asctime,
-                        "level": record.levelname,
-                        "execution_id": self.execution_id,
-                        "control_plane_id": self.control_plane_id,
-                        "task": self.NAME,
-                    },
-                    **get_error_telemetry(record.exc_info),
-                }
-            )
-            for record in self._logs
-        ]
+                        **{k: str(v) for k, v in record.__dict__.items() if k.lower() not in IGNORED_LOG_EXTRAS},
+                        **{
+                            "message": record.getMessage(),
+                            "ddsource": "azure",
+                            "service": "lfo",
+                            "time": record.asctime,
+                            "level": record.levelname,
+                            "execution_id": self.execution_id,
+                            "control_plane_id": self.control_plane_id,
+                            "task": self.NAME,
+                        },
+                        **get_error_telemetry(record.exc_info),
+                    }
+                )
+                for record in self._logs
+            ]
+        )
         self._logs.clear()
-        dd_metric = MetricSeries(
-            metric=CONTROL_PLANE_METRIC_PREFIX + "runtime_seconds",
-            points=[MetricPoint(timestamp=int(self.start_time), value=time() - self.start_time)],
-            tags=self.tags,
+        dd_metrics = MetricPayload(
+            series=[
+                MetricSeries(
+                    metric=CONTROL_PLANE_METRIC_PREFIX + "runtime_seconds",
+                    points=[MetricPoint(timestamp=int(self.start_time), value=time() - self.start_time)],
+                    tags=self.tags,
+                )
+            ]
         )
         await gather(
-            self._logs_client.submit_log(HTTPLog(value=dd_logs), ddtags=",".join(self.tags)),  # type: ignore
-            self._metrics_client.submit_metrics(MetricPayload(series=[dd_metric])),  # type: ignore
+            self._logs_client.submit_log(dd_logs, ddtags=",".join(self.tags)),  # type: ignore
+            self._metrics_client.submit_metrics(dd_metrics),  # type: ignore
         )
 
 
