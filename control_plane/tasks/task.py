07e79ca2610b508ddddf93e807a4fac91d7ec355
Matt Spurlin <matt.spurlin@datadoghq.com>
2025-05-12 17:21:32 -0400

    only change servers when staging targetted

--- control_plane/tasks/task.py
+++ control_plane/tasks/task.py
@@ -104,10 +104,11 @@ class Task(AbstractAsyncContextManager["Task"]):
         self._logs: list[LogRecord] = []
         configuration = Configuration()
 
-        if self.telemetry_enabled:
-            if "datad0g.com" in environ.get(DD_SITE_SETTING, ""):
-                configuration.server_index = 2
-                configuration.server_variables["site"] = "datad0g.com"
+        target_staging = self.telemetry_enabled and "datad0g.com" in environ.get(DD_SITE_SETTING, "")
+
+        if target_staging:
+            configuration.server_index = 2
+            configuration.server_variables["site"] = "datad0g.com"
 
             host_settings = configuration.get_host_settings()
             _add_datadog_staging(host_settings)
@@ -116,13 +117,14 @@ class Task(AbstractAsyncContextManager["Task"]):
         self._datadog_client = AsyncApiClient(configuration)
         self._logs_client = LogsApi(self._datadog_client)
         self._metrics_client = MetricsApi(self._datadog_client)
-        if self.telemetry_enabled:
+        if target_staging:
             logs_servers = self._logs_client._submit_log_endpoint.settings.get("servers")
             _add_datadog_staging(logs_servers)
 
             metrics_servers = self._metrics_client._submit_metrics_endpoint.settings.get("servers")
             _add_datadog_staging(metrics_servers)
 
+        if self.telemetry_enabled:
             log.info("Telemetry enabled, will submit logs for %s", self.NAME)
             self.log.addHandler(ListHandler(self._logs))
 
