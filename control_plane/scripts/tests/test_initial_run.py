d564e4ca84db80efe5246cb9be72bad94275aa37
Matt Spurlin <matt.spurlin@datadoghq.com>
2025-05-16 15:20:06 -0400

    add initial run test

--- control_plane/scripts/tests/test_initial_run.py
+++ control_plane/scripts/tests/test_initial_run.py
@@ -4,11 +4,10 @@
 
 # stdlib
 from os import environ
-from unittest.mock import AsyncMock
 
 # project
 from scripts.initial_run import main
-from tasks.tests.common import AsyncTestCase
+from tasks.tests.common import AsyncMockClient, AsyncTestCase
 
 
 def get_config_option(setting: str) -> str:
@@ -21,12 +20,41 @@ class TestMain(AsyncTestCase):
         environ["RESOURCE_GROUP"] = "test_resource_group"
         self.is_initial_deploy = self.patch_path("scripts.initial_run.is_initial_deploy")
         self.default_credential = self.patch_path("scripts.initial_run.DefaultAzureCredential")
-        self.container_apps_client = AsyncMock()
-        self.container_apps_client_patch = self.patch_path(
-            "scripts.initial_run.ContainerAppsAPIClient", return_value=self.container_apps_client
-        )
+        self.container_apps_client = AsyncMockClient()
+        self.patch_path("scripts.initial_run.ContainerAppsAPIClient", return_value=self.container_apps_client)
         self.get_config_option = self.patch_path("scripts.initial_run.get_config_option")
         self.get_config_option.side_effect = get_config_option
+
+        self.datadog_client = AsyncMockClient()
+        self.patch_path("scripts.initial_run.DatadogClient", return_value=self.datadog_client)
+
+        self.resources_task = AsyncMockClient()
+        self.patch_path("scripts.initial_run.ResourcesTask", return_value=self.resources_task)
+
+        self.scaling_task = AsyncMockClient()
+        self.patch_path("scripts.initial_run.ScalingTask", return_value=self.scaling_task)
+
+        self.diagnostic_settings_task = AsyncMockClient()
+        self.patch_path("scripts.initial_run.DiagnosticSettingsTask", return_value=self.diagnostic_settings_task)
+
+    async def test_main_runs_all_tasks_on_initial_run(self):
+        # GIVEN
+        self.is_initial_deploy.return_value = True
+        self.scaling_task.assignment_cache = {}
+
+        # WHEN
+        await main()
+
+        # THEN
+        self.container_apps_client.jobs.begin_start.assert_awaited_once()
+        self.assertTrue(self.get_config_option.called)
+        self.assertTrue(self.default_credential.called)
+
+        self.resources_task.run.assert_awaited_once()
+        self.scaling_task.run.assert_awaited()
+        self.diagnostic_settings_task.run.assert_awaited_once()
+
+        self.datadog_client.submit_status_update.assert_awaited()
 
     async def test_main_runs_deployer_on_follow_up_runs(self):
         # GIVEN
@@ -36,5 +64,4 @@ class TestMain(AsyncTestCase):
         await main()
 
         # THEN
-        # self.container_apps_client.jobs.begin_start.assert_awaited()
-        assert True
+        self.container_apps_client.jobs.begin_start.assert_awaited()
