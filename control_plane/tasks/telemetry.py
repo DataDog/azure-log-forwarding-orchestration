bdb27b0e1425f282a11277ac63cc5aa179f29cf0
Ava Silver <ava.silver@datadoghq.com>
2025-07-01 15:00:00 -0400

    [SVLS-7079] header

--- control_plane/tasks/telemetry.py
+++ control_plane/tasks/telemetry.py
@@ -1,5 +1,11 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2 License.
+
+# This product includes software developed at Datadog (https://www.datadoghq.com/) Copyright 2025 Datadog, Inc.
+
+# stdlib
 from os import environ
 
+# project
 from cache.env import DD_API_KEY_SETTING, DD_TELEMETRY_SETTING, is_truthy
 
 TELEMETRY_ENABLED = bool(is_truthy(DD_TELEMETRY_SETTING) and environ.get(DD_API_KEY_SETTING))
