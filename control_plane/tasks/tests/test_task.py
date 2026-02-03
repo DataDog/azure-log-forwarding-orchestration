7739d55aa27c94d5b5f5f6a90d98cdc871aa6844
Paul Caponetti <paul.caponetti@datadoghq.com>
2026-02-03 17:02:30 -0500

    probe for region

--- control_plane/tasks/tests/test_task.py
+++ control_plane/tasks/tests/test_task.py
@@ -53,7 +53,10 @@ class DummyTask(Task):
 class TestTask(TaskTestCase):
     async def asyncSetUp(self) -> None:
         await super().asyncSetUp()
+<<<<<<< Updated upstream
         self.cred = self.patch_path("tasks.common.DefaultAzureCredential").return_value
+=======
+>>>>>>> Stashed changes
         basicConfig(level=INFO)
         task_module.TELEMETRY_ENABLED = False
 
